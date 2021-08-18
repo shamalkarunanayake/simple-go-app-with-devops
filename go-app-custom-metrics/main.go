7b86990ff70cde5e22615111ff37c5d96f68f582
shamal karunanayake <shamalkarunanayake@gmail.com>
2021-08-19 02:16:35 +0530

    wave 8

--- go-app-custom-metrics/main.go
+++ go-app-custom-metrics/main.go
@@ -46,27 +46,37 @@ var (
 func main() {
 	log.Print("Logging in Go!")
 	log.SetFormatter(&log.JSONFormatter{})
-
-	standardFields := log.Fields{
-		"hostname": "staging-1",
-		"appname":  "foo-app",
-		"session":  "1ce3f6v",
+	for a := 0; a < 1000; a++ {
+		log.Print("Logging in Go!%d\n", a)
 	}
 
-	log.WithFields(standardFields).WithFields(log.Fields{"string": "foo", "int": 1, "float": 1.1}).Info("My first ssl event from Golang")
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+		"size":   10,
+	}).Info("A group of walrus emerges from the ocean")
 
-	histogramVec := prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name: "prom_request_time",
-		Help: "Time it has taken to retrieve the metrics",
-	}, []string{"time"})
+	log.WithFields(log.Fields{
+		"omg":    false,
+		"number": 11,
+	}).Warn("The group's number increased tremendously!")
 
-	prometheus.Register(histogramVec)
+	//log.WithFields(log.Fields{
+	//	"omg":    false,
+	//	"number": 102,
+	//}).Fatal("The ice breaks!")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello Demo, %q", html.EscapeString(r.URL.Path))
+	// A common pattern is to re-use fields between logging statements by re-using
+	// the logrus.Entry returned from WithFields()
+	contextLogger := log.WithFields(log.Fields{
+		"common": "this is a common field",
+		"other":  "I also should be logged always",
 	})
-	http.Handle("/metrics", newHandlerWithHistogram(promhttp.Handler(), histogramVec))
 
+	contextLogger.Info("I'll be logged with common and other field")
+	contextLogger.Info("Me too")
+	for b := 0; b < 1000; b++ {
+		log.Print("Counter logs !%d\n", b)
+	}
 	prometheus.MustRegister(counter)
 	prometheus.MustRegister(gauge)
 	prometheus.MustRegister(histogram)
@@ -82,6 +92,27 @@ func main() {
 			time.Sleep(time.Second)
 		}
 	}()
+
+	standardFields := log.Fields{
+		"hostname": "staging-1",
+		"appname":  "go-app",
+		"session":  "1ce3f6v",
+	}
+
+	log.WithFields(standardFields).WithFields(log.Fields{"string": "foo", "int": 1, "float": 1.1}).Info("My first ssl event from Golang")
+
+	histogramVec := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "prom_request_time",
+		Help: "Time it has taken to retrieve the metrics",
+	}, []string{"time"})
+
+	prometheus.Register(histogramVec)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi there, I love Devops %s!", html.EscapeString(r.URL.Path))
+	})
+	http.Handle("/metrics", newHandlerWithHistogram(promhttp.Handler(), histogramVec))
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
